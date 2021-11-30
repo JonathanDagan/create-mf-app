6530329fd30adec3a7ae35765f9c5f2b788378c5
Jonathan Dagan <jonathandagan12@gmail.com>
2021-11-30 23:45:20 +0200

    added void return types

--- src/builder.ts
+++ src/builder.ts
@@ -15,25 +15,25 @@ interface IReplacements {
     CONTAINER?: string,
 }
 
-const templateFile = (fileName: string, replacements: IReplacements) => {
-    let contents = fs.readFileSync(fileName, 'utf8').toString()
-    Object.keys(replacements).forEach((key) => {
-        contents = contents.replace(
+const templateFile = (fileName: string, replacements: IReplacements): void => {
+  let contents = fs.readFileSync(fileName, 'utf8').toString()
+  Object.keys(replacements).forEach((key) => {
+    contents = contents.replace(
             new RegExp(`(\{\{${key}\}\}|\{\{ ${key} \}\})`, 'g'), //eslint-disable-line
-            // TODO: fix this and remove ts ignore
-            //@ts-ignore
-            replacements[key]
-        )
-    })
-    fs.writeFileSync(fileName, contents)
+      // TODO: fix this and remove ts ignore
+      // @ts-ignore
+      replacements[key]
+    )
+  })
+  fs.writeFileSync(fileName, contents)
 }
 
 // required for npm publish
-const renameGitignore = (projectName: string) => {
-    const projectPath = path.join(__dirname, `../${projectName}`)
-    if (fs.existsSync(`${projectPath}/gitignore`)) {
+const renameGitignore = (projectName: string): void => {
+  const projectPath = path.join(__dirname, `../${projectName}`)
+  if (fs.existsSync(`${projectPath}/gitignore`)) {
         fs.renameSync(`${projectPath}/gitignore`, `${projectPath}/.gitignore`) //eslint-disable-line
-    }
+  }
 }
 
 // Options:
@@ -45,7 +45,7 @@ const renameGitignore = (projectName: string) => {
 //   - port: Port to run the project on
 
 interface IBuilder {
-    type: "Application" | "Library" | "Server",
+    type: 'Application' | 'Library' | 'Server',
     name: string,
     framework: string,
     language: 'ts' | 'js',
@@ -53,76 +53,75 @@ interface IBuilder {
     port: number,
 }
 
-module.exports = async ({ type, language, framework, name, css, port }: IBuilder) => {
+module.exports = async ({ type, language, framework, name, css, port }: IBuilder): Promise<void> => {
+  const replacements: IReplacements = {
+    NAME: name,
+    FRAMEWORK: framework,
+    SAFE_NAME: name.replace(/-/g, '_').trim(),
+    LANGUAGE: language
+  }
 
-    const replacements: IReplacements = {
-        NAME: name,
-        FRAMEWORK: framework,
-        SAFE_NAME: name.replace(/-/g, '_').trim(),
-        LANGUAGE: language
+  const tempDir = type.toLowerCase()
+
+  if (type === 'Library') {
+    await ncp(path.join(__dirname, `../templates/${tempDir}/typescript`), name, () => { }) // TODO: change callback to convention
+  }
+
+  if (type === 'Server') {
+    replacements.PORT = port
+
+    await ncp(
+      path.join(__dirname, `../templates/${tempDir}/${framework}`),
+      name,
+      () => { } // TODO: change callback to convention
+    )
+  }
+
+  if (type === 'Application') {
+    await ncp(
+      path.join(__dirname, `../templates/${tempDir}/${framework}/base`),
+      name,
+      () => { } // TODO: change callback to convention
+    )
+    await ncp(
+      path.join(__dirname, `../templates/${tempDir}/${framework}/${replacements.LANGUAGE}`),
+      name,
+      () => { } // TODO: change callback to convention
+    )
+
+    const tailwind = css === 'Tailwind'
+    replacements.CSS_EXTENSION = tailwind ? 'scss' : 'css'
+    replacements.CONTAINER = tailwind
+      ? 'mt-10 text-3xl mx-auto max-w-6xl'
+      : 'container'
+    replacements.CSS = tailwind ? 'Tailwind' : 'Empty CSS'
+    replacements.PORT = port
+
+    if (tailwind) {
+      fs.unlinkSync(path.join(name, '/src/index.css'))
+
+      await ncp(
+        path.join(__dirname, '../templates/application-extras/tailwind'),
+        name,
+        () => { } // TODO: change callback to convention
+      )
+
+      const packageJSON = JSON.parse(
+        fs.readFileSync(path.join(name, 'package.json'), 'utf8')
+      )
+      packageJSON.devDependencies.tailwindcss = '^2.0.2'
+      fs.writeFileSync(
+        path.join(name, 'package.json'),
+        JSON.stringify(packageJSON, null, 2)
+      )
     }
+  }
 
-    const tempDir = type.toLowerCase()
+  renameGitignore(name)
 
-    if (type === 'Library') {
-        await ncp(path.join(__dirname, `../templates/${tempDir}/typescript`), name, () => { }) // TODO: change callback to convention
+  glob.sync(`${name}/**/*`).forEach((file) => {
+    if (fs.lstatSync(file).isFile()) {
+      templateFile(file, replacements)
     }
-
-    if (type === 'Server') {
-        replacements.PORT = port
-
-        await ncp(
-            path.join(__dirname, `../templates/${tempDir}/${framework}`),
-            name,
-            () => { } // TODO: change callback to convention
-        )
-    }
-
-    if (type === 'Application') {
-        await ncp(
-            path.join(__dirname, `../templates/${tempDir}/${framework}/base`),
-            name,
-            () => { } // TODO: change callback to convention
-        )
-        await ncp(
-            path.join(__dirname, `../templates/${tempDir}/${framework}/${replacements.LANGUAGE}`),
-            name,
-            () => { } // TODO: change callback to convention
-        )
-
-        const tailwind = css === 'Tailwind'
-        replacements.CSS_EXTENSION = tailwind ? 'scss' : 'css'
-        replacements.CONTAINER = tailwind
-            ? 'mt-10 text-3xl mx-auto max-w-6xl'
-            : 'container'
-        replacements.CSS = tailwind ? 'Tailwind' : 'Empty CSS'
-        replacements.PORT = port
-
-        if (tailwind) {
-            fs.unlinkSync(path.join(name, '/src/index.css'))
-
-            await ncp(
-                path.join(__dirname, '../templates/application-extras/tailwind'),
-                name,
-                () => { } // TODO: change callback to convention
-            )
-
-            const packageJSON = JSON.parse(
-                fs.readFileSync(path.join(name, 'package.json'), 'utf8')
-            )
-            packageJSON.devDependencies.tailwindcss = '^2.0.2'
-            fs.writeFileSync(
-                path.join(name, 'package.json'),
-                JSON.stringify(packageJSON, null, 2)
-            )
-        }
-    }
-
-    renameGitignore(name)
-
-    glob.sync(`${name}/**/*`).forEach((file) => {
-        if (fs.lstatSync(file).isFile()) {
-            templateFile(file, replacements)
-        }
-    })
+  })
 }
