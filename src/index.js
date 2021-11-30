53e5f53acbea3c7ef6b1d4a1e2cd6ec3b30cb47a
Jonathan Dagan <jonathandagan12@gmail.com>
2021-11-30 22:55:02 +0200

    linting

--- src/index.js
+++ src/index.js
@@ -18,11 +18,11 @@ const templateFile = (fileName, replacements) => {
 
 // required for npm publish
 const renameGitignore = (projectName) => {
-  const projectPath = path.join(__dirname, `../${projectName}`);
+  const projectPath = path.join(__dirname, `../${projectName}`)
   if (fs.existsSync(`${projectPath}/gitignore`)) {
-    fs.renameSync(`${projectPath}/gitignore`, `${projectPath}/.gitignore`);
+    fs.renameSync(`${projectPath}/gitignore`, `${projectPath}/.gitignore`)
   }
-};
+}
 
 // Options:
 //   - type: "Application", "Library", "Server"
