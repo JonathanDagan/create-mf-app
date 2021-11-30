aeaf5ff71784bbb8775aa9b621be7094b0dd65d0
Jonathan Dagan <jonathandagan12@gmail.com>
2021-12-01 00:05:27 +0200

    inital file with types

---
+++ bin/create-mf-app.ts
@@ -0,0 +1,108 @@
+import inquirer from 'inquirer'
+import shell from 'shelljs'
+import fs from 'fs'
+import path from 'path'
+
+import Builder from '../src/builder'
+const builder = Builder;
+
+(async function () {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Pick the name of your app:',
+            name: 'name',
+            default: 'host'
+        },
+        {
+            type: 'list',
+            message: 'Project Type:',
+            name: 'type',
+            choices: ['Application', 'API Server', 'Library'],
+            default: 'Application'
+        }
+    ])
+
+    if (answers.type === 'Library') {
+        builder.mainFunction(answers)
+    }
+
+    if (answers.type === 'API Server') {
+        const templates = fs
+            .readdirSync(path.join(__dirname, '../templates/server'))
+            .sort()
+
+        const serverAnswers = await inquirer.prompt([
+            {
+                type: 'input',
+                message: 'Port number:',
+                name: 'port',
+                default: '8080'
+            },
+            {
+                type: 'list',
+                message: 'Template:',
+                name: 'framework',
+                choices: templates,
+                default: 'express'
+            }
+        ])
+
+        builder.mainFunction({
+            ...answers,
+            ...serverAnswers,
+            type: 'Server',
+            language: 'typescript'
+        })
+    }
+
+    if (answers.type === 'Application') {
+        const templates = fs
+            .readdirSync(path.join(__dirname, '../templates/application'))
+            .sort()
+
+        const appAnswers = await inquirer.prompt([
+            {
+                type: 'input',
+                message: 'Port number:',
+                name: 'port',
+                default: '8080'
+            },
+            {
+                type: 'list',
+                message: 'Framework:',
+                name: 'framework',
+                choices: templates,
+                default: 'react'
+            },
+            {
+                type: 'list',
+                message: 'Language:',
+                name: 'language',
+                choices: ['typescript', 'javascript'],
+                default: 'javascript'
+            },
+            {
+                type: 'list',
+                message: 'CSS:',
+                name: 'css',
+                choices: ['CSS', 'Tailwind'],
+                default: 'CSS'
+            }
+        ])
+
+        builder.mainFunction({
+            ...answers,
+            ...appAnswers
+        })
+    }
+
+    shell.echo(`Your '${answers.name}' project is ready to go.
+
+Next steps:
+
+▶️ cd ${answers.name}
+▶️ npm install
+▶️ npm start
+`)
+})()
